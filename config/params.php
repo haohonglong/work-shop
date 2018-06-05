3c7e1e985668066e0a8b2f31712d7bacba68731c
haohonglong <947923503@qq.com>
2018-06-06 00:02:59 +0800

    modify

--- config/params.php
+++ config/params.php
@@ -1,18 +1,5 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-	'personCardType'=>[
-		'1'=>'家长卡',
-		'2'=>'学生卡',
-		'3'=>'老人卡',
-	],
-    'SEX'=>[
-        '0'=>'女',
-        '1'=>'男',
-    ],
-    'relationType'=>[
-        '1'=>'文章',
-        '2'=>'视频',
-    ]
+    'adminEmail' => 'admin@example.com'
 ];
