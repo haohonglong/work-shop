35a8bab0c81cce1f302822ccc540841c5c90445d
haohonglong <947923503@qq.com>
2018-06-08 20:14:22 +0800

    add

--- helper/Helper.php
+++ helper/Helper.php
@@ -34,4 +34,25 @@ class Helper
         }
         return $str;
     }
+
+    /**
+     * @author lhh
+     * 创建日期：2018-06-08
+     * 修改日期：2018-06-08
+     * 名称：encrypt
+     * 功能：加密测试
+     * 说明：
+     * 注意：
+     * @param $id
+     * @param $data
+     * @return string
+     */
+    public static function encrypt($id,&$data){
+        $id=serialize($id);
+        $key="1112121212121212121212";
+        $data['iv']=base64_encode(substr('fdakinel;injajdji',0,16));
+        $data['value']=openssl_encrypt($id, 'AES-256-CBC',$key,0,base64_decode($data['iv']));
+        $encrypt=base64_encode(json_encode($data));
+        return $encrypt;
+    }
 }
