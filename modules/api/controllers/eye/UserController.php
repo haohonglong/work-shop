1760f813bcb186a264f972fccd6a6a3b508479e7
haohonglong <947923503@qq.com>
2018-06-08 21:57:24 +0800

    登陆测试

--- modules/api/controllers/eye/UserController.php
+++ modules/api/controllers/eye/UserController.php
@@ -8,6 +8,8 @@
 
 namespace app\modules\api\controllers\eye;
 
+use app\helper\Helper;
+use app\models\WechatApp;
 use yii;
 use app\helper\Response;
 use app\models\EyeUser;
@@ -20,6 +22,12 @@ class UserController extends BaseController
     {
         $model = new LoginForm();
         if ($model->load(\Yii::$app->request->post(),'')) {
+//            $model->user_info = 'test';
+//            $model->iv = 'ZmRha2luZWw7aW5qYWpkag==';
+//            $model->signature = Helper::getrandomstring(24);
+//            $model->encrypted_data = Helper::getrandomstring(24);
+            $model->wechat_app = WechatApp::findOne(2);
+
             $data = $model->login();
             return Response::json($data['code'],$data['msg'],$data['data']);
         }
